150047bb0a183806401ed5cfda7899fc523270b8
Gilbert B Garza <gilbertbgarza@gmail.com>
2010-04-15 00:27:48 -0500

    - Added categories by year view

--- app/controllers/data_view_controller.rb
+++ app/controllers/data_view_controller.rb
@@ -26,12 +26,65 @@ class DataViewController < ApplicationController
     @by_year = @db.view('comp_obj_category/year_month?group_level=2&reverse=true')
     @points = {}
     @months = %w{ Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec }
+    @years = []
     
     for doc in @by_year['rows']
       @cat = doc['key'][0]
-      puts doc['key'][1]
+      @years.push(doc['key'][1]) if not @years.include? doc['key'][1]
       @points[@cat] ||= []
-      @points[@cat].push(doc['value'].to_i)
+      @points[@cat].push(doc['value'].to_f / 1000000)
+    end
+    @years = @years.map { |y| y.to_s }
+    
+    @data = []
+    for cat, pset in @points
+      @data.push({
+        'name' => cat,
+        'data' => pset
+      })
+    end
+    
+    @options = {
+      :chart => {
+        :defaultSeriesType => 'line'
+      },
+      :title => { :text => 'Dollars Spent per Comptroller Object Category per Year' },
+      :xAxis => {
+        :categories => @years,
+        :title => { :text => 'Year' }
+      },
+      :yAxis => {
+        :title => { :text => 'Dollars Spent (Millions)' }
+      },
+      :series => @data
+    }
+    
+    @response = {
+      :status => 'ok',
+      :options => @options
+    }
+    
+    render :json => @response
+  end
+  
+  def by_month
+    @year = params[:year]
+    if @year.nil? or not [2007,2008,2009].include? @year.to_i
+      @response = { :status => 'bad year' }
+      return render :json => @response
+    end
+    @year = @year.to_i
+    
+    @db = CouchRest.database!("http://texasgov.info:5984/tceq_data")
+    @by_year = @db.view('comp_obj_category/year_month?group_level=3&reverse=true')
+    @points = {}
+    @months = %w{ Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec }
+    
+    for doc in @by_year['rows']
+      next if @year != doc['key'][1]
+      @cat = doc['key'][0]
+      @points[@cat] ||= []
+      @points[@cat].push(doc['value'].to_f / 1000000)
     end
     
     @data = []
@@ -42,7 +95,27 @@ class DataViewController < ApplicationController
       })
     end
     
-    render :json => @data
+    @options = {
+      :chart => {
+        :defaultSeriesType => 'line'
+      },
+      :title => { :text => 'Dollars Spent per Comptroller Object Category per Year' },
+      :xAxis => {
+        :categories => @months,
+        :title => { :text => 'Month (%d)' % @year }
+      },
+      :yAxis => {
+        :title => { :text => 'Dollars Spent (Millions)' }
+      },
+      :series => @data
+    }
+    
+    @response = {
+      :status => 'ok',
+      :options => @options
+    }
+    
+    render :json => @response
   end
 
 end
